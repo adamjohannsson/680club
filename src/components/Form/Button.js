838d76072128ddfab13e3f25255ab61beec0183c
juanmartinzzz <juan.martinzzz@gmail.com>
2024-09-30 23:40:25 -0500

    680-6-b Hotfix conflict resolution (#7)
    Co-authored-by: Juan M <juanm@juan.local>

--- src/components/Form/Button.js
+++ src/components/Form/Button.js
@@ -1,7 +1,12 @@
 function Button({ text, textSize, textDisabled, disabled, onClick }) {
-    return (
-        <div className={`Button TextSize ${textSize} ${disabled ? "disabled" : ""}`} onClick={onClick}>{disabled ? textDisabled || text : text}</div>
-    );
+  return (
+    <div
+      className={`Button TextSize ${textSize} ${disabled ? 'disabled' : ''}`}
+      onClick={onClick}
+    >
+      {disabled ? textDisabled || text : text}
+    </div>
+  );
 }
 
 export default Button;
