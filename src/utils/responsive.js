da674f8a75b9000f6d1842753a96b38fb841206c
Juan M <juan@examplemail.com>
2025-01-06 20:36:10 -0500

    CLUB-56 Formatting.

--- src/utils/responsive.js
+++ src/utils/responsive.js
@@ -1,6 +1,6 @@
 const isWeb = () => {
   // WARNING: if you change this value, also modify CSS class '.desktop-box' max-width property
-  return window.innerWidth > 768 ? true : false;
+  return window.innerWidth > 767 ? true : false;
 };
 
 const responsive = {
